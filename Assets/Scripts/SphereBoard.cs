23674886f4f9d619dbdd4c82b8a82c03699e1122
anggunw <56246449+anggunw@users.noreply.github.com>
2022-03-14 22:13:15 +0700

    node bug fix

--- Assets/Scripts/SphereBoard.cs
+++ Assets/Scripts/SphereBoard.cs
@@ -7,17 +7,17 @@ public class SphereBoard : MonoBehaviour
     public GameObject nodeCanvas;
     [SerializeField] private GameObject playerRef;
 
-    void Start()
-    {
-        playerRef = GameObject.Find("Stage Object/OVRCameraRig/TrackingSpace/CenterEyeAnchor").transform.GetChild(0).gameObject;
-        if(playerRef == null){
-            Manager.instance.PrintDebug("null");
-            playerRef = GameObject.Find("Stage Object/[VRSimulator_CameraRig]");
-        }
-    }
+    // void Start()
+    // {
+    //     playerRef = GameObject.Find("Stage Object/OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+    //     if(playerRef == null){
+    //         Manager.instance.PrintDebug("null");
+    //         playerRef = GameObject.Find("Stage Object/[VRSimulator_CameraRig]");
+    //     }
+    // }
 
-    void Update()
-    {
-        nodeCanvas.transform.LookAt(nodeCanvas.transform.position + playerRef.transform.rotation * Vector3.forward, playerRef.transform.rotation * Vector3.up);
-    }
+    // void Update()
+    // {
+    //     nodeCanvas.transform.LookAt(nodeCanvas.transform.position + playerRef.transform.rotation * Vector3.forward, playerRef.transform.rotation * Vector3.up);
+    // }
 }
