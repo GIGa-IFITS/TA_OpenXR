9fc62963a965ac92e87e6ffa36915611047efacc
Anggun Wahyuni <56246449+anggunw@users.noreply.github.com>
2022-05-31 11:49:10 +0700

    more stable gyro

--- Assets/Scripts/SmartphoneGyro.cs
+++ Assets/Scripts/SmartphoneGyro.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SmartphoneGyro : MonoBehaviour
 {
     public static SmartphoneGyro instance;
+    private GameObject gyroControl;
     private Quaternion rot;
-    public int newX;
-    public int newY;
-    public int newZ;
+    private Quaternion correctionRot;
+    public float correctionX;
+    public float correctionY;
+    public float correctionZ;
 
 
     private void Awake()
@@ -24,14 +26,24 @@ public class SmartphoneGyro : MonoBehaviour
         }
     }
 
+    private void Start(){
+        gyroControl = transform.parent.gameObject;
+        correctionRot = new Quaternion(0, 0, 1, 0);
+    }
+
+    private void LateUpdate() {
+        gyroControl.transform.rotation = Quaternion.Euler(correctionX, correctionY, correctionZ);
+    }
+
     public void SetPhoneRotation(float x, float y, float z, float w){
         rot.x = x;
         rot.y = y;
         rot.z = z;
         rot.w = w;
 
-        rot = Quaternion.Euler(newX, newY, newZ) * rot; 
-        transform.rotation = rot;
+        //transform.rotation = Quaternion.Euler(correctionX, correctionY, correctionZ) * rot; 
+        transform.localRotation = correctionRot * rot;
     }
+
 }
 
