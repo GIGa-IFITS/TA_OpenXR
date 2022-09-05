092f86481ef6aa4b438c3c333ffa8fc3815b743e
Anggun Wahyuni <56246449+anggunw@users.noreply.github.com>
2022-09-05 16:38:14 +0700

    toggle passthrough, move back button position in search menu

---
+++ Assets/Scripts/TogglePassthrough.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePassthrough : MonoBehaviour
+{
+    [SerializeField] private OVRPassthroughLayer ovrPassthroughLayer;
+
+    public void setPassthroughVisibility(){
+        ovrPassthroughLayer.hidden = !ovrPassthroughLayer.hidden;
+    }
+}
